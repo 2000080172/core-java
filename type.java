7216f4132b237ef40b0be5ad4b869bc5da3a35d8
2000080172 <81341236+2000080172@users.noreply.github.com>
2021-10-07 15:00:11 +0530

    Add files via upload

---
+++ type.java
@@ -0,0 +1,17 @@
+
+class Sum  {
+static double add(int a,int b)
+    {
+        return a+b;
+    }
+static double add(double a,double b)
+    {
+        return a+b;
+    }
+
+}
+public class SuperEx {
+    public static void main(String args[]) {
+    System.out.println(Sum.add(17,13));
+    }
+}

c6d3a9afb8cdf0e1fc59a5063851e0ef827fd4b8
2000080172 <81341236+2000080172@users.noreply.github.com>
2021-10-21 14:16:10 +0530

    Add files via upload

---
+++ pi.java
@@ -0,0 +1,18 @@
+class Sample{
+final double pi;
+public Sample()
+{
+pi=3.14;
+}
+public Sample(double pi)
+{
+this.pi=pi;
+}
+}
+public class MyClass {
+public static void main(String args[]) {
+Sample ob=new Sample(22/7);
+
+System.out.println(ob.pi);
+}
+}

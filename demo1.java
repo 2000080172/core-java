caa3c3d68cf47b7768c41f3f61d3ec2b629c3d57
2000080172 <81341236+2000080172@users.noreply.github.com>
2021-10-28 15:08:14 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class demo1 {
+    public static void main(String args[]) 
+        {
+            Scanner sc=new Scanner(System.in);
+      int x=100;
+      int y=sc.nextInt();
+      int z=x/y;
+      System.out.println(args[1]);
+   
+}
+}
